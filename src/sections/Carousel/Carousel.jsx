1985eac79d32500148afad15df77c79ceece734f
Alan Marcelo Flores <69022647+AlanMFlores@users.noreply.github.com>
2024-01-12 22:00:11 -0300

    CategoryCard image route modified

--- src/sections/Carousel/Carousel.jsx
+++ src/sections/Carousel/Carousel.jsx
@@ -5,10 +5,10 @@ import CategoryCard from '../../components/CategoryCard/CategoryCard'
 const Carousel = () => {
   return (
     <div className='carousel'>
-        <CategoryCard link={"/public/images/sports-card.jpg"} category={"SPORTS"} id={"sports"}/>
-        <CategoryCard link={"/public/images/home-card.jpg"} category={"HOME"} id={"home"}/>
-        <CategoryCard link={"/public/images/electronics-card.jpg"} category={"ELECTRONICS"} id={"electronics"}/>
-        <CategoryCard link={"/public/images/fashion-card.jpg"} category={"FASHION"} id={"fashion"}/>
+        <CategoryCard link={"./public/images/sports-card.jpg"} category={"SPORTS"} id={"sports"}/>
+        <CategoryCard link={"./public/images/home-card.jpg"} category={"HOME"} id={"home"}/>
+        <CategoryCard link={"./public/images/electronics-card.jpg"} category={"ELECTRONICS"} id={"electronics"}/>
+        <CategoryCard link={"./public/images/fashion-card.jpg"} category={"FASHION"} id={"fashion"}/>
     </div>
   )
 }
